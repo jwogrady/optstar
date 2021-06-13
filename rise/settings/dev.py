c670a92413ba64c78e2ad49ce64deff84eef6b8f
jwogrady <john@status26.com>
2021-06-13 14:48:43 -0500

    wagtail boilerplate

---
+++ rise/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-9dq+j))xy@!!qn*ll2ek^iv$@r+rz0*!o_n_^ls$blpz!mjf^+'
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ['*'] 
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
